0ad0a46ce876ae4d53cf4ca004b190e019586a4c
Nicholas Grogg <oddone9139@OldBoy>
2020-03-24 09:10:53 -1000

    Started laying out pseudocode in the auto solver

--- auto_solver.c
+++ auto_solver.c
@@ -2,3 +2,22 @@
 #include <stdio.h>
 
 // This is an automatic solver that expands on the manual_solver.c program
+
+// Function to shuffle the array(s)
+// Function to alter the cube
+// Function to check if the cube is completed (All sides just one color)
+// Function to output the cube, for visual progress
+
+int main() {
+	// Instantiate the six arrays
+	char s1[9] = {'R','R','R','R','R','R','R','R','R'};
+	char s2[9] = {'Y','Y','Y','Y','Y','Y','Y','Y','Y'};
+	char s3[9] = {'W','W','W','W','W','W','W','W','W'};
+	char s4[9] = {'G','G','G','G','G','G','G','G','G'};
+	char s5[9] = {'B','B','B','B','B','B','B','B','B'};
+	char s6[9] = {'O','O','O','O','O','O','O','O','O'};
+
+	// Shuffle the array
+	// Solve the array
+	return 0;
+}
