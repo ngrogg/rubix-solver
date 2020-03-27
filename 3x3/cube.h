4151acb5d0be01d07ba2759787252435e36e7ec0
Akil Marshall <akilmarshall@gmail.com>
2020-03-27 02:08:38 -1000

    further abstracted some of the good idea and removed a lot of ugly code

--- 3x3/cube.h
+++ 3x3/cube.h
@@ -1,145 +1,103 @@
 #include <stdlib.h>
 
 typedef struct cube_tile {
-    struct cube_tile *up;
+    // struct to represent a tile face of the Rubik cube
+    struct cube_tile *up_vertical;
+    struct cube_tile *up_horizontal;
+    struct cube_tile *across;
     struct cube_tile *right;
     struct cube_tile *left;
     struct cube_tile *down;
     char color;
-    int index;
 } cube_tile;
 
-typedef struct rubik_cube {
-    cube_tile *red_face;
-    cube_tile *blue_face;
-    cube_tile *white_face;
-    cube_tile *green_face;
-    cube_tile *yellow_face;
-    cube_tile *orange_face;
-} rubik_cube;
-
 cube_tile* Tile(char color) {
+    // constructor function for cube_tile struct
     cube_tile *temp = NULL;
     temp = (cube_tile *) malloc(sizeof(cube_tile));
-    temp->up = NULL;
+    temp->up_vertical = NULL;
+    temp->up_horizontal = NULL;
     temp->right = NULL;
     temp->down = NULL;
     temp->left = NULL;
     temp->color = color;
-    temp->index = 0;
+
     return temp;
 }
 
-cube_tile *create_face(char color){
-    cube_tile *corner = Tile(color);
-    cube_tile *top = corner;
-    for (int i = 0; i < 8; ++i) {
-        cube_tile *next = Tile(color);
-        corner->right = next;
-        next->left = corner;
-        corner = next;
-    }
-    corner->right = top;
-    top->left = corner;
-    return top;
+typedef struct face {
+    //  struct to represent a face of the Rubik cube
+    //  a b c
+    //  h ┼ d
+    //  g f e
+    //  are the tiles that make up the face they are doubly linked around 
+    //  the perimeter using the left and right pointers and across the 
+    //  center using the across pointers.
+    cube_tile *a;
+    cube_tile *b;
+    cube_tile *c;
+    cube_tile *d;
+    cube_tile *e;
+    cube_tile *f;
+    cube_tile *g;
+    cube_tile *h;
+    char color;
+} face;
+
+face *Face(char color) {
+    // constructor function for face struct
+    face *temp = NULL;
+    temp = (face *) malloc(sizeof(face));
+
+    temp->color = color;
+
+    temp->a = Tile(color);
+    temp->b = Tile(color);
+    temp->c = Tile(color);
+    temp->d = Tile(color);
+    temp->e = Tile(color);
+    temp->f = Tile(color);
+    temp->g = Tile(color);
+    temp->h = Tile(color);
+
+    temp->a->right = temp->b;
+    temp->a->left = temp->h;
+    temp->b->right = temp->c;
+    temp->b->left = temp->a;
+    temp->c->right = temp->d;
+    temp->c->left = temp->b;
+    temp->d->right = temp->e;
+    temp->d->left = temp->c;
+    temp->e->right = temp->f;
+    temp->e->left = temp->d;
+    temp->f->right = temp->g;
+    temp->f->left = temp->e;
+    temp->g->right = temp->h;
+    temp->g->left = temp->f;
+    temp->h->right = temp->a;
+    temp->h->left = temp->g;
+
+    temp->b->across = temp->f;
+    temp->f->across = temp->b;
+    temp->h->across = temp->d;
+    temp->d->across = temp->h;
+
+    return temp;
 }
 
-void cycle(cube_tile *head, int n) {
-    // shift a face by n.
-    // positive is rightwards, negative is leftwards
-    if (n > 0) {
-        for (int i = 0; i < n; ++i) {
-            head = head->right;
-        }
-    } else if (n < 0) {
-        for (int i = 0; i < n; ++i) {
-            head = head->left;
-        }
-    }
-}
+typedef struct rubik_cube {
+    // struct to represent the Rubik cube
+    face *red_face;
+    face *blue_face;
+    face *white_face;
+    face *green_face;
+    face *yellow_face;
+    face *orange_face;
+} rubik_cube;
 
-cube_tile *__add_face(cube_tile *face_a, cube_tile *face_b) {
-    //  a helper function to be used in construction of a cube object
-    //  NOT intended for general use
-    cube_tile *top = face_a;
-    for (int i = 0; i < 3; ++i) {
-        face_a->up = face_b;
-        face_b->down = face_a;
-        face_a = face_a->right;
-        face_b = face_b->right;
-    }
-    return top;
-}
-
-cube_tile *_add_blue_face(cube_tile *corner, rubik_cube *cube) {
-    // helper function for create_cube.
-    // this function is NOT intended for general use.
-    cube_tile *blue_face = create_face('B');
-    cube->blue_face = blue_face;
-    corner = __add_face(corner, blue_face);
-    return corner;
-}
-
-cube_tile *_add_white_face(cube_tile *corner, rubik_cube *cube) {
-    // helper function for create_cube.
-    // this function is NOT intended for general use.
-    cube_tile *white_face = create_face('W');
-    cube->white_face = white_face;
-    cycle(corner, 3);
-    corner = __add_face(corner, white_face);
-    return corner;
-}
-
-cube_tile *_add_green_face(cube_tile *corner, rubik_cube *cube) {
-    // helper function for create_cube.
-    // this function is NOT intended for general use.
-    cube_tile * green_face = create_face('G');
-    cube->green_face = green_face;
-    cycle(corner, 6);
-    corner = __add_face(corner, green_face);
-    return corner;
-}
-
-cube_tile *_add_yellow_face(cube_tile *corner, rubik_cube *cube) {
-    // helper function for create_cube.
-    // this function is NOT intended for general use.
-    cube_tile * yellow_face = create_face('Y');
-    cube->yellow_face = yellow_face;
-    cycle(corner, 9);
-    corner = __add_face(corner, yellow_face);
-    return corner;
-}
-
-cube_tile *_add_orange_face(cube_tile *corner, rubik_cube *cube) {
-    // helper function for create_cube.
-    // this function is NOT intended for general use.
-    cube_tile *orange_face = create_face('O');
-    cube->orange_face = orange_face;
-}
-
-rubik_cube *create_cube() {
-    rubik_cube * cube = NULL;
-    cube = (rubik_cube *) malloc(sizeof(rubik_cube));
-
-    cube_tile *corner = create_face('R');
-    for (int i = 0; i < 5; ++i) {
-        switch (i) {
-            case 0:
-                corner = _add_blue_face(corner, cube);
-                break;
-            case 1:
-                corner = _add_white_face(corner, cube);
-                break;
-            case 2:
-                corner = _add_green_face(corner, cube);
-                break;
-            case 3:
-                corner = _add_yellow_face(corner, cube);
-                break;
-            case 4:
-                corner = _add_orange_face(corner, cube);
-                break;
-        }
-    }
+rubik_cube *Rubik_Cube() {
+    // constructor function for rubik_cube struct
+    rubik_cube *cube = Rubik_Cube();
+    // TODO
     return cube;
 }
